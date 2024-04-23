cd7695adfa23a77b91df08cad0742c16c4c50ef5
Luna Nova <git@lunnova.dev>
2024-04-22 19:33:45 -0700

    lun/discord: always add armcord as well as stock electron discord

--- users/lun/gui/discord.nix
+++ users/lun/gui/discord.nix
@@ -1,37 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
   discordPathSuffix = "";
   extractCmd = pkgs.makeBinaryWrapper.extractCmd or (pkgs.writeShellScript "extract-binary-wrapper-cmd" ''
     strings -dw "$1" | sed -n '/^makeCWrapper/,/^$/ p'
   '');
+  openAsarDiscord = (pkgs.discord.override {
+    withOpenASAR = true;
+    withVencord = false;
+  }).overrideAttrs (old: {
+    postInstall = ''
+      ${old.postInstall}
+      if grep '\0' $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} && wrapperCmd=$(${extractCmd} $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix}) && [[ $wrapperCmd ]]; then
+        # Binary wrapper
+        parseMakeCWrapperCall() {
+          shift # makeCWrapper
+          oldExe=$1; shift
+          oldWrapperArgs=("$@")
+        }
+        eval "parseMakeCWrapperCall ''${wrapperCmd//"$out"/"$out"}"
+        # Binary wrapper
+        makeWrapper $oldExe $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} "''${oldWrapperArgs[@]}" --add-flags "${extraElectronArgs}"
+      else
+        # Normal wrapper
+        substituteInPlace $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} \
+        --replace '"$@"' '${extraElectronArgs} "$@"'
+      fi
+    '';
+  });
 in
 {
-  home.packages =
-    if (pkgs.system == "x86_64-linux") then
-      [
-        ((pkgs.discord.override {
-          withOpenASAR = true;
-          withVencord = false;
-        }).overrideAttrs (old: {
-          postInstall = ''
-            ${old.postInstall}
-            if grep '\0' $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} && wrapperCmd=$(${extractCmd} $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix}) && [[ $wrapperCmd ]]; then
-              # Binary wrapper
-              parseMakeCWrapperCall() {
-                shift # makeCWrapper
-                oldExe=$1; shift
-                oldWrapperArgs=("$@")
-              }
-              eval "parseMakeCWrapperCall ''${wrapperCmd//"$out"/"$out"}"
-              # Binary wrapper
-              makeWrapper $oldExe $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} "''${oldWrapperArgs[@]}" --add-flags "${extraElectronArgs}"
-            else
-              # Normal wrapper
-              substituteInPlace $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} \
-              --replace '"$@"' '${extraElectronArgs} "$@"'
-            fi
-          '';
-        }))
-      ] else [ pkgs.armcord ];
+  home.packages = [ pkgs.armcord ]
+    ++ lib.optional (pkgs.system == "x86_64-linux") openAsarDiscord;
 }
