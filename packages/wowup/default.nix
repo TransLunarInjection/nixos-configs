caab7c48084704c1b930e58a32734cbcca4b503a
Luna Nova <git@lunnova.dev>
2024-04-22 19:24:18 -0700

    wowup: update to CF-2.10.0

--- packages/wowup/default.nix
+++ packages/wowup/default.nix
@@ -3,8 +3,8 @@ appimageTools.wrapType2 {
   # or wrapType1
   name = "wowup";
   src = fetchurl {
-    url = "https://github.com/WowUp/WowUp.CF/releases/download/v2.9.1-beta.8/WowUp-CF-2.9.1-beta.8.AppImage";
-    hash = "sha256-sdBOAlaiovWQlzv1QIFdEdp0BySd8EmHxwq8w0gfLPk=";
+    url = "https://github.com/WowUp/WowUp.CF/releases/download/v2.10.0/WowUp-CF-2.10.0.AppImage";
+    hash = "sha256-u8rziod2RVSCaqSBgShqFeVrRo9MvHr7cCujSAYpULM=";
   };
   profile = ''
     export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
