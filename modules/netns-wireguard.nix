e5130dbe07e7d6cbaf422e508eb29134c1408c34
Luna Nova <git@lunnova.dev>
2024-11-01 07:07:53 -0700

    netns-wireguard: iproute moved to iproute2

--- modules/netns-wireguard.nix
+++ modules/netns-wireguard.nix
@@ -141,11 +141,11 @@ in
               PrivateMounts = false;
               ExecStart = (pkgs.writeShellScript "netns-up" ''
                 set -xe
-                ${pkgs.iproute}/bin/ip netns add $1
+                ${pkgs.iproute2}/bin/ip netns add $1
                 ${pkgs.utillinux}/bin/umount /var/run/netns/$1
                 ${pkgs.utillinux}/bin/mount --bind /proc/self/ns/net /var/run/netns/$1
               '') + " %I";
-              ExecStop = "${pkgs.iproute}/bin/ip netns del %I";
+              ExecStop = "${pkgs.iproute2}/bin/ip netns del %I";
             };
           };
 
