466c4c33fc95b23cde554e132a81f63942b4da51
Luna Nova <git@lunnova.dev>
2024-11-01 07:06:26 -0700

    sway: only enable on personal profile

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,29 +1,31 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  xdg.portal = {
-    enable = true;
-    wlr = {
+  config = lib.mkIf (config.lun.profiles.graphical && config.lun.profiles.personal) {
+    xdg.portal = {
       enable = true;
+      wlr = {
+        enable = true;
+      };
     };
-  };
 
-  programs.sway = {
-    enable = true;
-    extraOptions = [ "--unsupported-gpu" ];
-    wrapperFeatures = {
-      base = true;
-      gtk = false;
+    programs.sway = {
+      enable = true;
+      extraOptions = [ "--unsupported-gpu" ];
+      wrapperFeatures = {
+        base = true;
+        gtk = false;
+      };
+      extraPackages = with pkgs; [
+        swaylock
+        swayidle
+        xwayland
+        wl-clipboard
+        mako # notification daemon
+        foot # foot is the default terminal in the config
+        dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+        wofi
+        kanshi # sway monitor settings / autorandr equivalent? https://github.com/RaitoBezarius/nixos-x230/blob/764d2237ab59ded81492b6c76bc29da027e9fdb3/sway.nix example using it
+      ];
     };
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      xwayland
-      wl-clipboard
-      mako # notification daemon
-      foot # foot is the default terminal in the config
-      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-      wofi
-      kanshi # sway monitor settings / autorandr equivalent? https://github.com/RaitoBezarius/nixos-x230/blob/764d2237ab59ded81492b6c76bc29da027e9fdb3/sway.nix example using it
-    ];
   };
 }
