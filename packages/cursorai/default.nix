460b37ba51c89c9035256841ae073ed2f18b25d8
Luna Nova <git@lunnova.dev>
2024-11-01 07:04:47 -0700

    cursorai: bump sha

--- packages/cursorai/default.nix
+++ packages/cursorai/default.nix
@@ -5,7 +5,7 @@ appimageTools.wrapType2 {
   name = "cursor-ai";
   src = fetchurl {
     url = "https://downloader.cursor.sh/linux/appImage/x64";
-    hash = "sha256-qF9vqfvGRGDJ4dZxYzvRFdIKxt6ieiQXupPiOzkF4us=";
+    hash = "sha256-GWkilBlpXube//jbxRjmKJjYcmB42nhMY8K0OgkvtwA=";
   };
   profile = ''
     export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
