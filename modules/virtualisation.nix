5230d7a3ebfef3614dd9b5e4eee5afb85f4b1225
Luna Nova <git@lunnova.dev>
2024-11-15 17:47:28 -0800

    virtualisation: always turn off boot.enableContainers

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,39 +1,43 @@
 { pkgs, config, lib, ... }:
 {
   options.lun.virtualisation.enable = lib.mkEnableOption "virt" // { default = true; };
-  config = lib.mkIf config.lun.virtualisation.enable {
-    # Not using NixOS containers currently
-    boot.enableContainers = false;
+  config = lib.mkMerge [
+    {
+      # Not using NixOS containers currently
+      boot.enableContainers = false;
+    }
+    (lib.mkIf config.lun.virtualisation.enable {
 
-    virtualisation = {
-      # FIXME: rootless podman keeps fucking up so disabling it for now
-      # podman = {
-      #   enable = true;
-      #   dockerCompat = true; # docker alias
-      # };
-      docker.enable = true;
-      oci-containers.backend = "docker";
-    };
-
-    lun.persistence.dirs = [
-      "/var/lib/docker"
-      "/var/lib/containers"
-      "/var/lib/libvirt"
-    ];
-
-    environment.systemPackages = with pkgs; [
-      virt-manager
-      virtiofsd
-    ];
-
-    virtualisation.libvirtd = {
-      enable = true;
-      qemu = {
-        ovmf.enable = true;
-        runAsRoot = false;
+      virtualisation = {
+        # FIXME: rootless podman keeps fucking up so disabling it for now
+        # podman = {
+        #   enable = true;
+        #   dockerCompat = true; # docker alias
+        # };
+        docker.enable = true;
+        oci-containers.backend = "docker";
       };
-      onBoot = "ignore";
-      onShutdown = "shutdown";
-    };
-  };
+
+      lun.persistence.dirs = [
+        "/var/lib/docker"
+        "/var/lib/containers"
+        "/var/lib/libvirt"
+      ];
+
+      environment.systemPackages = with pkgs; [
+        virt-manager
+        virtiofsd
+      ];
+
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+          ovmf.enable = true;
+          runAsRoot = false;
+        };
+        onBoot = "ignore";
+        onShutdown = "shutdown";
+      };
+    })
+  ];
 }
