615b7a930742af63f69791820a38fa39da7eacd9
Luna Nova <git@lunnova.dev>
2024-11-15 17:50:46 -0800

    persistence: systemd initrd automatic dir creation

--- modules/persistence/default.nix
+++ modules/persistence/default.nix
@@ -1,5 +1,5 @@
 # Based off setup from github:buckley310/nixos-config although has diverged now
-{ config, lib, ... }:
+{ config, lib, utils, ... }:
 let
   cfg = config.lun.persistence;
   inherit (config.lun.persistence) persistPath;
@@ -8,6 +8,31 @@ let
   isNonEmpty = s: (builtins.match "[ \t\n]*" s) == null;
   absolutePathWithoutTrailingSlash = addCheckDesc "absolute path without trailing slash" lib.types.str
     (s: isNonEmpty s && (builtins.match "/.+/" s) == null);
+  directories = config.lun.persistence.dirs;
+  persistentStoragePaths = lib.unique directories;
+  all = config.fileSystems; # // config.virtualisation.fileSystems;
+  getDevice = fs: if fs.device != null then fs.device else "/dev/disk/by-label/${fs.label}";
+  matchFileSystems = fs: lib.attrValues (lib.filterAttrs (_: v: v.mountPoint or null == fs) all);
+  fileSystems = lib.concatMap matchFileSystems persistentStoragePaths;
+  deviceUnits = lib.unique
+    (map
+      (fs:
+        if fs.fsType == "zfs" then
+          "zfs-import.target"
+        else
+          "${(utils.escapeSystemdPath (getDevice fs))}.device")
+      (lib.filter (fs: fs.neededForBoot && !(builtins.elem "bind" fs.options)) (builtins.attrValues config.fileSystems)));
+
+  createNeededForBootDirs = ''
+    for bdir in ${builtins.concatStringsSep " " cfg.dirs}; do
+      if [ -d /sysroot/persist ]; then
+        echo mkdir -p "/sysroot/persist$bdir"
+        mkdir -p "/sysroot/persist$bdir"
+      else
+        echo "Can't create /sysroot/persist$bdir because sysroot not mounted!!!"
+      fi
+    done
+  '';
 in
 {
   options.lun.persistence = {
@@ -59,9 +84,65 @@ in
       let escapedFiles = builtins.map (lib.escape [ "\"" "\\" ]) cfg.files;
       in
       builtins.map (path: "L+ \"${path}\" - - - - ${persistPath}${path}") escapedFiles;
+    # services.journald.extraConfig = ''
+    #     ForwardToConsole=yes
+    #     MaxLevelConsole=debug
+    #   '';
+    boot.initrd = lib.mkIf config.boot.initrd.systemd.enable {
+      #systemd.enable = true;
+      systemd.managerEnvironment = { SYSTEMD_LOG_LEVEL = "info"; };
+      systemd.contents."/etc/systemd/journald.conf".text = ''
+        [Journal]
+        ForwardToConsole=yes
+        MaxLevelConsole=info
+      '';
+      systemd.extraConfig = ''
+        StatusUnitFormat=combined
+        LogLevel=info
+        DefaultTimeoutStartSec=30
+      '';
+      systemd.services = {
+        create-persist-dirs = {
+          wantedBy = [ "initrd-root-device.target" ];
+          # requires = deviceUnits;
+          # after = deviceUnits;
+          requires = [ "sysroot-persist.mount" ] ++ deviceUnits;
+          after = [ "sysroot-persist.mount" ] ++ deviceUnits;
+          serviceConfig.Type = "oneshot";
+          unitConfig.DefaultDependencies = false;
+          script = createNeededForBootDirs;
+        };
+      };
+      # postDeviceCommands = lib.mkIf (!config.boot.initrd.systemd.enable)
+      #   (lib.mkAfter createNeededForBootDirs);
+    };
 
     fileSystems =
-      let pathToFilesystem = name: { inherit name; value = { device = "${persistPath}${name}"; noCheck = true; neededForBoot = true; options = [ "bind" ]; }; };
-      in builtins.listToAttrs (builtins.map pathToFilesystem cfg.dirs);
+      let
+        pathToFilesystem = name: {
+          inherit name;
+          value = {
+            device = "${persistPath}${name}";
+            # fsType = "none";
+            noCheck = true;
+            neededForBoot = true;
+            # depends = [ "${persistPath}" ];
+            options = [
+              "bind"
+              # "x-systemd.requires-mounts-for=${persistPath}"
+              #"x-systemd.requires=create-persist-dirs"
+              # "x-systemd.requires-mounts-for=/sysroot${persistPath}"
+              # "x-systemd.requires=/sysroot${persistPath}"
+              # "x-systemd.automount"
+              # "X-fstrim.notrim"
+              # "x-gvfs-hide"
+              #"nofail"
+              #  "x-systemd.device-timeout=5s"
+              #  "x-systemd.mount-timeout=5s"
+            ];
+          };
+        };
+      in
+      builtins.listToAttrs (builtins.map pathToFilesystem cfg.dirs);
   };
 }
