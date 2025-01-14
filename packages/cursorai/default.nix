b80d9dbdad978113fb8352bdc1b6a1ac0652d120
Luna Nova <git@lunnova.dev>
2025-01-13 18:53:11 -0800

    cursor-ai: use pname, version

--- packages/cursorai/default.nix
+++ packages/cursorai/default.nix
@@ -2,10 +2,11 @@
 { appimageTools, fetchurl, gsettings-desktop-schemas, gtk3 }:
 appimageTools.wrapType2 {
   # or wrapType1
-  name = "cursor-ai";
+  pname = "cursor-ai";
+  version = "20241116203009";
   src = fetchurl {
-    url = "https://downloader.cursor.sh/linux/appImage/x64";
-    # url = "https://web.archive.org/web/20241116203009/https://downloader.cursor.sh/linux/appImage/x64";
+    # url = "https://downloader.cursor.sh/linux/appImage/x64";
+    url = "https://web.archive.org/web/20241116203009/https://downloader.cursor.sh/linux/appImage/x64";
     hash = "sha256-fr2P4Na6Jvmhh7FA5JILxrmm8wfI7Ad2+IFeJrxCtmI=";
   };
   profile = ''
